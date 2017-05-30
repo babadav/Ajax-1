319f659d4bf7a1ff117ce4a8dbf064ccf7adbf4c
babadav <babadav@gmail.com>
2017-05-29 23:05:20 -0400

    commit

--- wd4-ajax-twitter-master/js/week5.js
+++ wd4-ajax-twitter-master/js/week5.js
@@ -1,3 +1,41 @@
+
+
+var findRegex = (function(){
+
+    var shared = {}
+
+    function url(data){
+        var urlRegex = /((http|ftp|https)?:\/\/\S+)/g;
+        var firstRegex = data.replace(urlRegex,"<a href='$1' style='color:green'>$1</a>");
+
+        return firstRegex
+
+    }
+
+    function at(data){
+        var atRegex = /(^|\W)(@[a-z\d][\w-]*)/ig;
+        var secondRegex = data.replace(atRegex, "<a  href='$2' style='color:blue;'>$2</a>");
+
+        return secondRegex
+    }
+
+    function hash(data){
+        var hashRegex = /(^|\W)(#[a-z\d][\w-]*)/ig;
+        var thirdRegex = data.replace(hashRegex, "<a  href='$3' style='color:yellow;'>$3</a>")
+
+        return thirdRegex
+    }
+
+        shared = {
+            url: url,
+            at: at,
+            hash: hash
+        }
+
+        return shared
+})();
+
+
 var TwitterApi = (function(options) {
     var shared = {};
     var options = options || {};
@@ -47,7 +85,7 @@ var TwitterApi = (function(options) {
 
             	 var tweetText = document.createElement('p');
 
-            	 $(tweetText).html(findRegex(results[i].text));
+            	 // $(tweetText).html(findRegex(results[i].text));
 
             	 $('.results-container').append(tweetText);
             };
@@ -55,16 +93,28 @@ var TwitterApi = (function(options) {
 
      function processSearchTweets(result) {
         var results = JSON.parse(result).statuses;
-        // console.log(result);
 
         for (var i = 0; i < results.length; i++) {
             results[i]
             var timelineResult = document.createElement('p');
-            $(timelineResult).html(findRegex(results[i].text));
+            var a = findRegex.hash(results[i].text);
+            var b = findRegex.at(a);
+            var c = findRegex.url(b);
+            $(timelineResult).html(c) ;
             $('.results-container').append(timelineResult);
-        }
 
-    }
+            if (results[i].geo) {
+                var geoData = {
+                    lat: results[i].geo.coordinates[0],
+                    lng: results[i].geo.coordinates[1]
+                } 
+                GoogleModule.createMarker(c, geoData)
+                console.log(geoData);
+            } else{ console.log('whores');
+
+            }
+        };
+    };
 
     function removeTweets(){
     	for (var i = 0; i<$('p').length; i++) {
@@ -85,21 +135,55 @@ var TwitterApi = (function(options) {
     return shared;
 }());
 
-function findRegex(data) {
-        var urlRegex = /((http|ftp|https)?:\/\/\S+)/g;
-        var atRegex = /(^|\W)(@[a-z\d][\w-]*)/ig;
-        var hashRegex = /(^|\W)(#[a-z\d][\w-]*)/ig;
-        var firstRegex = data.replace(urlRegex,"<a href='$1' style='color:green'>$1</a>");
-        var secondRegex = firstRegex.replace(atRegex, "<a  href='$2' style='color:blue;'>$2</a>");
-        console.log(secondRegex);
-        return secondRegex
-    }
 
 TwitterApi.init();
 
 
+var GoogleModule = (function(){
+    var shared = {};
+
+    function createMarker(tweet, geoData){
+
+        var infowindow = new google.maps.InfoWindow({
+          content: tweet
+        });
+
+        var marker= new google.maps.Marker({
+          position: geoData,
+          map: map
+        });
+
+        marker.addListener('click', function(){
+          infowindow.open(map, marker);
+        });
 
 
+    }
+
+    function init() {
+       
+        var ccLatLng = {lat:33.813245, lng:-84.362171};
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: ccLatLng,
+          zoom: 8
+        });
+
+          createMarker("balls", ccLatLng);
+    };
+
+    shared = {
+        init: init,
+        createMarker: createMarker
+    };
+
+    return shared
+
+
+
+}());
+
+        
+ 
 
 
 
